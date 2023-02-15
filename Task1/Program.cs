6e09b7109204d4bcbc99b6d18cd3c2bba2abe36a
GRIGORY <bondarenko_grisha@vk.com>
2023-02-15 19:09:40 +0700

    create task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+ 
+ int GetNumberA(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+        break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+int GetNumberB(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+        break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+int GetResult(int numberA, int numberB)
+{
+ int result = 1;
+
+    for (int i = 0; i < numberB; i++)
+    {
+    result *= numberA;
+    }
+
+return result;
+}
+
+int numberA = GetNumberA("Введите число А:");
+int numberB = GetNumberB("Введите число B:");
+int result = GetResult(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
